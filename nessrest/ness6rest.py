a18c6e11af1e863f242fd120d3de98c84675521a
Anne Kobata <kobata.anne@gmail.com>
2016-07-26 09:59:06 -0700

    download HTML chapers
    	modified:   nessrest/ness6rest.py

--- nessrest/ness6rest.py
+++ nessrest/ness6rest.py
@@ -909,13 +909,15 @@ class Scanner(object):
         return kbs
 
 ################################################################################
-    def download_scan(self, export_format="nessus", dbpasswd=""):
+    def download_scan(self, export_format="", chapters="", dbpasswd=""):
         running = True
         counter = 0
 
         self.action("scans/" + str(self.scan_id), method="get")
         if (export_format=="db"):
             data = {"format":"db","password":dbpasswd}
+        elif (export_format=="html"):
+            data = {"format":export_format,"chapters":chapters}
         else:
             data = {'format': export_format}
         self.action("scans/" + str(self.scan_id) + "/export",
